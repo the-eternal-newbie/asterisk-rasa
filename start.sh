5b0ab4df4d07ff9410396820d2ba91bcbd1dad8a
Carlos VP <ca.varaperez@gmail.com>
2021-07-01 13:41:49 -0500

    Adding TTS and SPT (this last one needs a better approach)

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 ##!/bin/bash
-
+DEEPSPEECH_DIR="./rasa-bot/deepspeech-models/"
 OPTION="$1"
 DOWN="down"
 CLEAN="clean"
@@ -12,5 +12,13 @@ else
         docker-compose down
     else
         docker-compose -f docker-compose.yml up -d && docker container exec -it -d rasa-bot bash -c "cd rasa && rasa run -m models --enable-api"
+        if [ -d "$DEEPSPEECH_DIR" ]; then
+            echo "Deepspeech already downloaded..."
+        else
+            cd ./rasa-bot/ && \
+                wget https://github.com/mozilla/DeepSpeech/releases/download/v0.5.1/deepspeech-0.5.1-models.tar.gz && \
+                tar xvfz deepspeech-0.5.1-models.tar.gz && mv deepspeech-0.5.1-models deepspeech-models && \
+                mv ._deepspeech-0.5.1-models ._deepspeech-models && rm deepspeech-0.5.1-models.tar.gz
+        fi
     fi
 fi
